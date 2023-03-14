9e5ce205bdc8f80609bf11ab2d6f4ac24574df6d
Tejash Patel <108259942+Tejash2911@users.noreply.github.com>
2023-03-14 10:12:11 +0530

    Add files via upload

---
+++ learnJS/1. Introduction To Javascript/script.js
@@ -0,0 +1,17 @@
+let c = 12;
+let d = 13;
+
+let age = 26; // initialization
+
+const name = 'Tejash';
+const Name = "Patel";
+
+// console.log('age', age)
+// age = 28;
+
+// console.log("c+d is", c + d);
+
+// console.log('age', age)
+console.log('name', name);
+
+
